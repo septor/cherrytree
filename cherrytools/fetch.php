b33514aa907602621dd0fd64084b7931a72c24d7
septor <patrickweaver@gmail.com>
2024-12-29 13:16:50 -0500

    let's display the required levels and possible rewards (basecamp only right now)

--- cherrytools/fetch.php
+++ cherrytools/fetch.php
@@ -12,6 +12,8 @@ if (isset($_GET['data']) || isset($_GET['questLine'])) {
     $totals = [];
     $expandedNeeds = [];
     $expandedQuestLevels = [];
+    $rewardTotals = [];
+    $highestLevels = [];
 
     foreach ($needs as $need) {
         if (strpos($need, '-') !== false) {
@@ -37,14 +39,29 @@ if (isset($_GET['data']) || isset($_GET['questLine'])) {
     $expandedQuestLevels = array_unique($expandedQuestLevels);
 
     foreach ($expandedNeeds as $need) {
-        if (isset($baseCampData[$need]['resources'])) {
-            $results[$need] = $baseCampData[$need]['resources'];
-            foreach ($baseCampData[$need]['resources'] as $key => $value) {
-                if (is_numeric($value)) {
+        if (isset($baseCampData[$need])) {
+            $levelData = $baseCampData[$need];
+            if (isset($levelData['resources'])) {
+                foreach ($levelData['resources'] as $key => $value) {
                     if (!isset($totals[$key])) {
                         $totals[$key] = 0;
                     }
                     $totals[$key] += $value;
+                }
+            }
+            if (isset($levelData['requirements'])) {
+                foreach ($levelData['requirements'] as $skill => $level) {
+                    if (!isset($highestLevels[$skill]) || $level > $highestLevels[$skill]) {
+                        $highestLevels[$skill] = $level;
+                    }
+                }
+            }
+            if (isset($levelData['rewards'])) {
+                foreach ($levelData['rewards'] as $reward => $amount) {
+                    if (!isset($rewardTotals[$reward])) {
+                        $rewardTotals[$reward] = 0;
+                    }
+                    $rewardTotals[$reward] += $amount;
                 }
             }
         }
@@ -54,26 +71,52 @@ if (isset($_GET['data']) || isset($_GET['questLine'])) {
         if (empty($expandedQuestLevels)) {
             $expandedQuestLevels = array_keys($questsData[$questLine]);
         }
-
         foreach ($expandedQuestLevels as $level) {
             if (isset($questsData[$questLine][$level])) {
                 foreach ($questsData[$questLine][$level] as $key => $value) {
-                    if (is_numeric($value)) {
-                        if (!isset($totals[$key])) {
-                            $totals[$key] = 0;
-                        }
-                        $totals[$key] += $value;
+                    if (!isset($totals[$key])) {
+                        $totals[$key] = 0;
                     }
+                    $totals[$key] += $value;
                 }
             }
         }
     }
 
-    if (empty($results) && empty($totals)) {
+    if (empty($totals) && empty($rewardTotals) && empty($highestLevels)) {
         echo "No data found.";
     } else {
+        echo "<table style='border-collapse: collapse; width: 100%; text-align: left;'>
+        <tr>
+        <th>Required Resources</th>
+        <th>Highest Required Levels</th>
+        <th>Total Rewards</th>
+        </tr>
+        <tr>
+        <td style='vertical-align: top;'>";
         foreach ($totals as $item => $total) {
             echo number_format($total) . "x " . $item . "<br>";
         }
+        echo "</td>
+        <td style='vertical-align: top;'>";
+        if(!empty($highestLevels)) {
+            foreach ($highestLevels as $skill => $level) {
+                echo "Level ".$level." ".$skill."<br>";
+            }
+        } else {
+            echo "No requirements needed.";
+        }
+        echo "</td>
+        <td style='vertical-align: top;'>";
+        if (!empty($rewardTotals)) {
+            foreach ($rewardTotals as $reward => $total) {
+                echo $total . "x " . $reward . "<br>";
+            }
+        } else {
+            echo "No rewards found.";
+        }
+        echo "</td>
+        </tr>
+        </table>";
     }
 }
